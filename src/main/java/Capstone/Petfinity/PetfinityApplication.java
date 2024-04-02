758de5304f0864059667fff5df92a024265f7e2a
yereumi <lovelyyeji01@gmail.com>
2024-04-03 00:50:43 +0900

    Initial commit

---
+++ src/main/java/Capstone/Petfinity/PetfinityApplication.java
@@ -0,0 +1,13 @@
+package Capstone.Petfinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetfinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetfinityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Capstone/Petfinity/PetfinityApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.Petfinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
