eb7f8ed0555ba700d5e190c50626d25e2a560cf3
yereumi <lovelyyeji01@gmail.com>
2024-04-08 16:42:57 +0900

    Parent API ing

--- src/main/java/Capstone/Petfinity/api/ParentApiController.java
+++ src/main/java/Capstone/Petfinity/api/ParentApiController.java
@@ -1,8 +1,15 @@
 package Capstone.Petfinity.api;
 
+import Capstone.Petfinity.domain.Parent;
 import Capstone.Petfinity.service.ParentService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,4 +17,27 @@ public class ParentApiController {
 
     private final ParentService parentService;
 
+    @PostMapping("/register/parent")
+    // 미완성
+    public RegisterParentResponse saveParent(@RequestBody RegisterParentRequest request) {
+
+        Parent parent = new Parent();
+        parent.setName(request.getName());
+        UUID uuid = parentService.register(parent);
+        return new RegisterParentResponse(uuid);
+    }
+
+    @Data
+    static class RegisterParentRequest {
+        private String name;
+
+    }
+    @Data
+    static class RegisterParentResponse {
+        private UUID uuid;
+
+        public RegisterParentResponse(UUID uuid) {
+            this.uuid = uuid;
+        }
+    }
 }
--- src/test/java/Capstone/Petfinity/service/ParentServiceTest.java
+++ src/test/java/Capstone/Petfinity/service/ParentServiceTest.java
@@ -6,7 +6,6 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.parameters.P;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
